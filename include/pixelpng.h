5da151511458bec66732e4a4644f4b3a8e0db58b
Nicolas Ucieda <nicolas.ucieda@gmail.com>
2025-11-27 21:59:39 +0100

    chore: used 1-byte ints instead of unsigned chars where it made sense

--- include/pixelpng.h
+++ include/pixelpng.h
@@ -4,6 +4,7 @@
 // https://www.libpng.org/pub/png/spec/1.2/PNG-Chunks.html
 
 # include <stddef.h>
+# include <stdint.h>
 # include <stdio.h>
 
 # define IHDR_CHUNK_LENGTH 13
@@ -27,22 +28,22 @@ enum chunk_type {
 // TODO perhaps worth optimizing since maximum size is 4 bytes, and we always have 6
 // I suppose grayscale is identical to r==g==b so that can be removed, and the palette can be "overriden" by alpha
 typedef struct {
-	unsigned char grayscale;	// 0-255
-	unsigned char red;		// 0-255
-	unsigned char green;		// 0-255
-	unsigned char blue;		// 0-255
-	unsigned char alpha;		// 0-255
-	unsigned char palette_index;	// 0-255
+	uint8_t grayscale;	// 0-255
+	uint8_t red;		// 0-255
+	uint8_t green;		// 0-255
+	uint8_t blue;		// 0-255
+	uint8_t alpha;		// 0-255
+	uint8_t palette_index;	// 0-255
 } pixel;
 
 typedef struct {
-	int width;				// pixels
-	int height;				// pixels
-	unsigned char bit_depth;		// Valid values are 1, 2, 4, 8, and 16
-	unsigned char color_type;		// Valid values are 0, 2, 3, 4, and 6
-	unsigned char compression_method;	// Only 0 is valid for now
-	unsigned char filter_method;		// Only 0 is valid for now
-	unsigned char interlace_method;		// 0 (no interlace) or 1 (Adam7 interlace)
+	int width;			// pixels
+	int height;			// pixels
+	uint8_t bit_depth;		// Valid values are 1, 2, 4, 8, and 16
+	uint8_t color_type;		// Valid values are 0, 2, 3, 4, and 6
+	uint8_t compression_method;	// Only 0 is valid for now
+	uint8_t filter_method;		// Only 0 is valid for now
+	uint8_t interlace_method;	// 0 (no interlace) or 1 (Adam7 interlace)
 } ChunkIHDR;
 
 typedef struct {
@@ -57,20 +58,20 @@ typedef struct {
 
 // PNG
 pixelPNG* initialize_png(int width, int height);
-void generate_png(pixelPNG* pixelPNG, char* file_name, unsigned char bit_depth, int color_type, unsigned char compression_method, unsigned char filter_method, unsigned char interlace_method);
+void generate_png(pixelPNG* pixelPNG, char* file_name, uint8_t bit_depth, int color_type, uint8_t compression_method, uint8_t filter_method, uint8_t interlace_method);
 void free_png(pixelPNG* pixelPNG);
 
 // Pixels
 pixel get_pixel(pixelPNG* pixelPNG, int x, int y);
-void set_pixel(pixelPNG* pixelPNG, int x, int y, unsigned char grayscale, unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha, unsigned char palette_index);
-void set_pixel_grayscale(pixelPNG* pixelPNG, int x, int y, short int grayscale);
-void set_pixel_grayscale_alpha(pixelPNG* pixelPNG, int x, int y, short int grayscale, short int alpha);
-void set_pixel_rgb(pixelPNG* pixelPNG, int x, int y, short int red, short int green, short int blue);
-void set_pixel_rgba(pixelPNG* pixelPNG, int x, int y, short int red, short int green, short int blue, short int alpha);
+void set_pixel(pixelPNG* pixelPNG, int x, int y, uint8_t grayscale, uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha, uint8_t palette_index);
+void set_pixel_grayscale(pixelPNG* pixelPNG, int x, int y, uint8_t grayscale);
+void set_pixel_grayscale_alpha(pixelPNG* pixelPNG, int x, int y, uint8_t grayscale, uint8_t alpha);
+void set_pixel_rgb(pixelPNG* pixelPNG, int x, int y, uint8_t red, uint8_t green, uint8_t blue);
+void set_pixel_rgba(pixelPNG* pixelPNG, int x, int y, uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha);
 
 
 // Generate Chunk Data
-ChunkIHDR* generate_ihdr_chunk(int width, int height, unsigned char bit_depth, int color_type, unsigned char compression_method, unsigned char filter_method, unsigned char interlace_method);
+ChunkIHDR* generate_ihdr_chunk(int width, int height, uint8_t bit_depth, int color_type, uint8_t compression_method, uint8_t filter_method, uint8_t interlace_method);
 ChunkIDAT* generate_idat_chunk(pixel** pixels, ChunkIHDR* ihdr_chunk);
 
 // Write Chunks
